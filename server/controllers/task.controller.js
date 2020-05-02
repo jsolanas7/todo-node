44256ab01208d21b466e6676519112d6a69c4a6a
Juancito <juanisolanas@gmail.com>
2020-05-02 17:50:31 -0300

    Delete compare encrypt

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const { createHandle } = require('../services/task/create.task.service');
-const { getUserByEmailHandle } = require('../services/user/getByEmail.user.service');
 const { validateToken } = require('../middlewares/authentication');
 
 
@@ -10,26 +9,6 @@ app.post('/task/create', [validateToken], async (req,res) => {
         await createHandle(req.body);
         res.json({
             ok:true
-        })
-    }catch(exc){
-        res.json({
-            ok: false,
-            message: exc.message
-        })
-    }
-})
-
-app.get('/task/getByDescription', [validateToken], async (req,res) => {
-    try{
-        const user = await getUserByEmailHandle(req.body);
-        res.json({
-            ok:true,
-            user: {
-                _id: user._id,
-                firstName: user.firstName,
-                surName: user.surName,
-                email: user.email
-            }
         })
     }catch(exc){
         res.json({
