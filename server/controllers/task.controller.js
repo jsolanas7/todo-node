bb0cbd36ff8e5c377cda5734ed9914f1bcafcc78
Juan Andrés Solanas <jsolanas@mstech.la>
2020-05-07 15:37:44 -0300

    Add controller to get tasks by user

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { createHandle } = require('../services/task/create.task.service');
+const { getByUserHandle } = require('../services/task/getTaskByUser.task');
 const { validateToken } = require('../middlewares/authentication');
 
 
@@ -9,6 +10,21 @@ app.post('/task/create', [validateToken], async (req,res) => {
         await createHandle(req.body);
         res.json({
             ok:true
+        })
+    }catch(exc){
+        res.json({
+            ok: false,
+            message: exc.message
+        })
+    }
+})
+
+app.get('/task/getByUser', [validateToken], async (req,res) => {
+    try{
+        const tasks = await getByUserHandle(req.body);
+        res.json({
+            ok:true,
+            tasks
         })
     }catch(exc){
         res.json({
